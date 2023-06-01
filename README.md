3a2c3418faee6199cab77a204e7fa4b207c58252
daeho <mnisdh2@gmail.com>
2023-06-01 12:45:52 +0900

    finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # typescript-bc
+
+### the typescript handbook
+ - https://www.typescriptlang.org/docs/handbook/intro.html
+
+npm i -D typescript
+npm i -D ts-node
+npm i nodemon
+
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import  crypto from "crypto";
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.ccalculateHash(prevHash, height, data);
+    }
+
+    static ccalculateHash(prevHash: string, height: number, data: string) {
+        const toHash = `${prevHash}${height}${data}`
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
+}
+
+class Blockchain {
+    private blocks: Block[];
+    constructor() {
+        this.blocks = [];
+    }
+
+    private getPrevHash() {
+        if(this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+
+    public addBlock(data: string) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+
+    public getBlocks() {
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock("first one");
+blockchain.addBlock("second one");
+blockchain.addBlock("third one");
+blockchain.addBlock("fourth one");
+
+console.log(blockchain.getBlocks());
